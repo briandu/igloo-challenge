4902cec1b5c8afae5701fb7a412088f4483430d1
Brian Du <brian.bh.du@gmail.com>
2017-06-16 15:18:07 -0400

    Added background image

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  document.querySelector('#js-btn-action-more').onclick = function(){
-    console.log("test")
-  };
+  $('#js-btn-action-more').on('click', function(e){
+    // e.preventDefault();
+    // console.log("test");
+    //
+    // $.ajax({
+    //   url: 'https://randomuser.me/api/',
+    //   dataType: 'json',
+    //   success: function(data) {
+    //     console.log(data);
+    //   };
+
+//     o = {};
+//     o.apibaseurl = "https://randomuser.me";
+//     o.apipath = "/api";
+//     a = new ApiGetClient(o).xhr.onreadystatechange = function () {
+//       var DONE = 4; // readyState 4 means the request is done.
+//       var OK = 200; // status 200 is a successful return.
+//       if (xhr.readyState === DONE) {
+//         if (xhr.status === OK) {
+//           console.log(xhr.responseText); // 'This is the returned text.'
+//         } else {
+//           console.log('Error: ' + xhr.status); // An error occurred during the request.
+//         }
+//       }
+//       //
+//       t = JSON.parse(a.response.text);
+//       console.log(t)
+//
+//     };
+// //
+//     // debugger
+
+  });
 
 });
