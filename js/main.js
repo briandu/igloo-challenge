09a70ef834b1eb357d7d2d3320d8df887e0f1766
Brian Du <brian.bh.du@gmail.com>
2017-06-17 15:06:33 -0400

    Added displayMember function

--- js/main.js
+++ js/main.js
@@ -1,48 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  $('#js-btn-action-more').click(function(){
+  var $members = $('#members')
+  function displayMembers(){
 
-    var $members = $('#members')
     $.ajax({
-      url: 'https://randomuser.me/api/',
+      url: 'https://randomuser.me/api/?results=10',
       dataType: 'json',
       success: function(members) {
         $.each(members, function(i, member){
-          // console.log(member[0].id.name)
-          // $members.append("<img src='" + member[0].picture.large +"' />")
-          $members.append("<img src='" + member[0].picture.large +"' /><li>"+ member[0].name.first +'</li><li>'+ member[0].name.last +'')
+          for (i = 0; i < 5; i++){
+            $members.append("<img src='" + member[i].picture.large +"' /><li>"+ member[i].name.first +'</li><li>'+ member[0].name.last +'')
+          }
         })
       }
     });
+
+  }
+
+  // This displays default 5 members
+  displayMembers();
+
+  // This calls for 5 additional members to be loaded
+  $('#js-btn-action-more').click(function(){
+    displayMembers();
   });
 
 });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//
-//   document.querySelector('#js-btn-action-more').onclick = function(){
-//
-//     o = {};
-//     o.apibaseurl = "https://randomuser.me";
-//     o.apipath = "/api";
-//     a = new ApiGetClient(o);
-//     // debugger
-//     t = JSON.parse(a.response.text);
-//     console.log(t)
-//
-//   };
-//
-// });
-
-// var DONE = 4; // readyState 4 means the request is done.
-// var OK = 200; // status 200 is a successful return.
-// if (xhr.readyState === DONE) {
-//   if (xhr.status === OK)
-//     console.log(xhr.responseText); // 'This is the returned text.'
-//   } else {
-//     console.log('Error: ' + xhr.status); // An error occurred during the request.
-//   }
-// }
-//
-// t = JSON.parse(a.response.text);
-// console.log(t)
