8eb6b84d45f15255fec9f7fd910909014fd244ae
Brian Du <brian.bh.du@gmail.com>
2017-06-17 14:34:26 -0400

    Added Jquery and set up users with AJAX

--- js/main.js
+++ js/main.js
@@ -1,37 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(){
 
-  $('#js-btn-action-more').on('click', function(e){
-    // e.preventDefault();
-    // console.log("test");
-    //
-    // $.ajax({
-    //   url: 'https://randomuser.me/api/',
-    //   dataType: 'json',
-    //   success: function(data) {
-    //     console.log(data);
-    //   };
+  $('#js-btn-action-more').click(function(){
 
-//     o = {};
-//     o.apibaseurl = "https://randomuser.me";
-//     o.apipath = "/api";
-//     a = new ApiGetClient(o).xhr.onreadystatechange = function () {
-//       var DONE = 4; // readyState 4 means the request is done.
-//       var OK = 200; // status 200 is a successful return.
-//       if (xhr.readyState === DONE) {
-//         if (xhr.status === OK) {
-//           console.log(xhr.responseText); // 'This is the returned text.'
-//         } else {
-//           console.log('Error: ' + xhr.status); // An error occurred during the request.
-//         }
-//       }
-//       //
-//       t = JSON.parse(a.response.text);
-//       console.log(t)
-//
-//     };
-// //
-//     // debugger
-
+    var $members = $('#members')
+    $.ajax({
+      url: 'https://randomuser.me/api/',
+      dataType: 'json',
+      success: function(members) {
+        $.each(members, function(i, member){
+          // console.log(member[0].id.name)
+          // $members.append("<img src='" + member[0].picture.large +"' />")
+          $members.append("<img src='" + member[0].picture.large +"' /><li>"+ member[0].name.first +'</li><li>'+ member[0].name.last +'')
+        })
+      }
+    });
   });
 
 });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//
+//   document.querySelector('#js-btn-action-more').onclick = function(){
+//
+//     o = {};
+//     o.apibaseurl = "https://randomuser.me";
+//     o.apipath = "/api";
+//     a = new ApiGetClient(o);
+//     // debugger
+//     t = JSON.parse(a.response.text);
+//     console.log(t)
+//
+//   };
+//
+// });
+
+// var DONE = 4; // readyState 4 means the request is done.
+// var OK = 200; // status 200 is a successful return.
+// if (xhr.readyState === DONE) {
+//   if (xhr.status === OK)
+//     console.log(xhr.responseText); // 'This is the returned text.'
+//   } else {
+//     console.log('Error: ' + xhr.status); // An error occurred during the request.
+//   }
+// }
+//
+// t = JSON.parse(a.response.text);
+// console.log(t)
