4ecdcdc0ea7c480c874ecd7c61e294f2764e91d1
Brian Du <brian.bh.du@gmail.com>
2017-06-17 17:01:35 -0400

    Fixed headings

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
-
-  var $members = $('#members')
+  var page = 1;
   function displayMembers(){
 
     $.ajax({
-      url: 'https://randomuser.me/api/?results=10',
+      url: 'https://randomuser.me/api/?results=5&page=' + page,
       dataType: 'json',
-      success: function(members) {
-        $.each(members, function(i, member){
-          for (i = 0; i < 5; i++){
-            $members.append("<img src='" + member[i].picture.large +"' /><li>"+ member[i].name.first +'</li><li>'+ member[0].name.last +'')
-          }
+      success: function(response) {
+        $('#members').append("<div class='row'></div>")
+        $.each(response.results, function(i, member){
+            console.log("member", member.picture)
+            $('#members .row:last-child').append("<div class='item'><img src='" + member.picture.large +"' /><h4>"+ member.name.first +' '+ member.name.last + '</h4><h5>'+ member.email +'<h5></div>')
         })
+        page++;
       }
     });
 
